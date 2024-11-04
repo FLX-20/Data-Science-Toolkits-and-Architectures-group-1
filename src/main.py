f037d5818caf23a8f73097dfc91572184f7b4116
Felix <fe.pappe@gmx.de>
2024-11-04 19:05:41 +0100

    added file for the evaluation function
    This file is for evaluation to assess the performance of
    the trained models.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
-import numpy as np
-import keras
-from keras import layers
 from data_loader import load_preprocess_data
 from models import build_mnist_cnn
 from train import train_model
-from config import batch_size, epochs
+from evaluate import evaluate_model
 
 # Load and preprocess data
 (x_train, y_train), (x_test, y_test) = load_preprocess_data()
@@ -15,11 +12,5 @@ model = build_mnist_cnn()
 # Train model
 train_model(model, x_train, y_train)
 
-
-"""
-## Evaluate the trained model
-"""
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
+# Evaluate model
+evaluate_model(model, x_test, y_test)
