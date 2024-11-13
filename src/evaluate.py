ea9eae0a5fdea359902fa187c45c542da0462501
Felix <fe.pappe@gmx.de>
2024-11-13 10:35:41 +0100

    added a function for single image classfication
    This function allows to use the trained model
    to classify single images by providing the file path.

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from tensorflow.keras.preprocessing import image as keras_image
 import numpy as np
 
 def evaluate_model(model, x_test, y_test):
@@ -19,3 +20,18 @@ def plot_confusion_matrix(classifier, test_data, test_labels):
     plt.title("Normalized Confusion Matrix")
     plt.savefig("confusion_matrix.png")
     print("Confusion matrix saved as confusion_matrix.png")
+
+def predict_image_label(model, image_path):
+
+    input_shape = model.input_shape[1:3] 
+    color_mode = "grayscale" if model.input_shape[-1] == 1 else "rgb"
+
+    img = keras_image.load_img(image_path, target_size=input_shape, color_mode=color_mode)
+    img_array = keras_image.img_to_array(img)
+    img_array = img_array.astype("float32") / 255.0 
+    img_array = np.expand_dims(img_array, axis=0) 
+
+    predictions = model.predict(img_array)
+    predicted_label = np.argmax(predictions, axis=1)[0] 
+
+    return predicted_label
