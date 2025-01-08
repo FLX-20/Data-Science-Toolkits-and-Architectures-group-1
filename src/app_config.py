9e72e3721406b3c6c0c98445dabdb699522e136d
Felix <fe.pappe@gmx.de>
2025-01-08 18:25:13 +0100

    adapted wandb training function
    THe function needed to be adapted to the new preprocessing pipeline.

--- src/app_config.py
+++ src/app_config.py
@@ -4,7 +4,7 @@ IMAGE_SAVE_PATH = "images"
 
 URL_TRAINING_DATA = "https://www.kaggle.com/api/v1/datasets/download/borhanitrash/animal-image-classification-dataset"
 DATASET_NAME = "MNIST"
-MODEL_NAME = "cnn_small_model"
+MODEL_NAME = "mnist_cnn_model"
 
 BATCHE_SIZE = 32
 EPOCHS = 10
